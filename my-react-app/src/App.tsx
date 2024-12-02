a1c285baf59cd63d43fdf2f6c37552c8e2faacfb
BrandonW <bran.wongsoft@hotmail.com>
2024-12-02 21:38:51 +0800

    commit with code

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+
+const App = () => {
+  return(
+    <div className="container">
+      <div>
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
